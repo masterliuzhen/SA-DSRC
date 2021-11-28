4887e1b98989910433aee04eea2510e81ec2baed
masterliuzhen <32841343+masterliuzhen@users.noreply.github.com>
2021-11-28 20:25:16 +0800

    Update SA_DSRC_demo.m

--- SA_DSRC_demo.m
+++ SA_DSRC_demo.m
@@ -2,9 +2,9 @@ clear all
 close all
 clc
 
-addpath ./OMPbox              % 将OMPbox添加到路径
-data = load('DataBase.mat');  % load the provided data   AR.mat        DataBase(YaleB)  
-DataBase = data.DataBase;     % DataBase = data.DataBase; 
+addpath ./OMPbox           
+data = load('DataBase.mat'); 
+DataBase = data.DataBase;   
 param.lambda =0.0001;        
-param.sparsity =28;          % 28
+param.sparsity =28;         
 SA_DSRC(DataBase, param);    
